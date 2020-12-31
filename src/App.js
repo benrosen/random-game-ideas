794fedf50ea5d4e466b8d00445340beb0816fe61
Benjamin Rosen <benjamin.p.rosen@gmail.com>
2020-12-31 17:59:07 -0500

    display idea in center of page; refresh idea on click

--- src/App.js
+++ src/App.js
@@ -1,3 +1,32 @@
+import { useCallback, useEffect, useState } from "react";
+
+import { Grid } from "semantic-ui-react";
+import getRandomIdea from "./getRandomIdea";
+
 export default function App() {
-  return <p>Hello world!</p>;
+  const [idea, setIdea] = useState();
+
+  const generate = useCallback(() => {
+    const newIdea = getRandomIdea();
+    setIdea(newIdea);
+  }, []);
+
+  useEffect(() => {
+    generate();
+  }, [generate]);
+
+  return (
+    <Grid
+      style={{ height: "100vh" }}
+      onClick={() => {
+        generate();
+      }}
+    >
+      <Grid.Row verticalAlign="middle">
+        <Grid.Column textAlign="center">
+          <p style={{ fontSize: "2em", fontFamily: "Josefin Sans" }}>{idea}</p>
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
+  );
 }
