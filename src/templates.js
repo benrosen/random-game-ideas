beb83049b49282285d5bc338395ad7ecd9feb8b7
Benjamin Rosen <benjamin.p.rosen@gmail.com>
2020-12-31 17:56:22 -0500

    create starter templates

---
+++ src/templates.js
@@ -0,0 +1,15 @@
+const templates = [
+  "you are {{ an_adjective }} {{ noun }}",
+  "use {{ an_adjective }} {{ noun }}",
+  "{{ nouns }} and {{ nouns }}",
+  "{{ a_noun }} vs {{ nouns }}",
+  "find {{ a_noun }}",
+  "watch out for {{ adjective }} {{ nouns }}",
+  "fighting {{ nouns }}",
+  "compete for {{ nouns }}",
+  "no {{ nouns }}",
+  "{{ nouns }}, but {{ adjective }}",
+  "{{ noun }} simulator",
+];
+
+export default templates;
