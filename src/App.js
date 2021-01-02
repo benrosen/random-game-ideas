4aee3dc720e65ddd46c6e04b045936442b4ec468
Benjamin Rosen <benjamin.p.rosen@gmail.com>
2021-01-01 22:33:49 -0500

    use random color pair for text and background colors

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import { useCallback, useEffect, useState } from "react";
 
 import { Grid } from "semantic-ui-react";
 import getRandomIdea from "./getRandomIdea";
+import useRandomColorPair from "./useRandomColorPair";
 
 export default function App() {
   const [idea, setIdea] = useState();
+  const [colorPair, randomize] = useRandomColorPair();
 
   const generate = useCallback(() => {
     const newIdea = getRandomIdea();
     setIdea(newIdea);
-  }, []);
+    randomize();
+  }, [randomize]);
 
   useEffect(() => {
     generate();
@@ -17,14 +20,25 @@ export default function App() {
 
   return (
     <Grid
-      style={{ height: "100vh" }}
+      style={{
+        backgroundColor: colorPair ? colorPair[0] : "#000000",
+        height: "100vh",
+      }}
       onClick={() => {
         generate();
       }}
     >
       <Grid.Row verticalAlign="middle">
         <Grid.Column textAlign="center">
-          <p style={{ fontSize: "2em", fontFamily: "Josefin Sans" }}>{idea}</p>
+          <p
+            style={{
+              color: colorPair ? colorPair[1] : "#ffffff",
+              fontSize: "2em",
+              fontFamily: "Josefin Sans",
+            }}
+          >
+            {idea}
+          </p>
         </Grid.Column>
       </Grid.Row>
     </Grid>
