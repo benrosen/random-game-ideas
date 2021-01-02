58eddea0eb6e17f1eb42a4c8888164672eedec2d
Benjamin Rosen <benjamin.p.rosen@gmail.com>
2021-01-01 22:32:36 -0500

    create useRandomColorPair hook

---
+++ src/useRandomColorPair.js
@@ -0,0 +1,19 @@
+import { useCallback, useEffect, useState } from "react";
+
+import getColorPair from "random-color-pair";
+
+export default function useRandomColorPair() {
+  const [colorPair, setColorPair] = useState();
+
+  const randomize = useCallback(() => {
+    const newPair = getColorPair();
+    console.log(newPair);
+    setColorPair(newPair);
+  }, []);
+
+  useEffect(() => {
+    randomize();
+  }, [randomize]);
+
+  return [colorPair, randomize];
+}
